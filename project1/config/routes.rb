e8ff79cf37c6bdd63d900d331d926c154ff913f1
sieunhan <nguyenminhduc1803@gmail.com>
2016-07-01 11:38:38 +0700

    authentication

--- project1/config/routes.rb
+++ project1/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   resources :categories
+  get 'signup'  => 'users#new' 
+  resources :users
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
--- project1/db/schema.rb
+++ project1/db/schema.rb
@@ -11,12 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701030552) do
+ActiveRecord::Schema.define(version: 20160701035500) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string   "title"
+    t.text     "meaning"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "words", ["category_id"], name: "index_words_on_category_id"
 
 end
